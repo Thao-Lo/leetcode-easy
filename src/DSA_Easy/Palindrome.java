10281d42f2fd8fab439ec7f470c1f7c5f05e5568
Zavis <thezavis@gmail.com>
2024-03-06 23:54:53 +1100

    Palindrome aand TwoSum

---
+++ src/DSA_Easy/Palindrome.java
@@ -0,0 +1,56 @@
+package DSA_Easy;
+
+import java.util.Scanner;
+
+public class Palindrome {
+	public static void main(String[] args) {
+
+		Scanner keyboard = new Scanner(System.in);
+
+		System.out.println(Math.ceil((double) 9 / 2));
+
+		System.out.print("Please input a number: ");
+		int inputNumber = keyboard.nextInt();
+
+		boolean isPalindrome = isNumberPalindrom(inputNumber);
+		System.out.println("Is Palindrome: " + isPalindrome);
+	}
+
+	public static boolean isInputNumberPalindrom(int inputNumber) {
+		String numberToString = Integer.toString(inputNumber);
+
+		char[] charArray = numberToString.toCharArray();
+		char[] reversedArray;
+
+		if (charArray.length % 2 == 0) {
+			reversedArray = new char[charArray.length / 2];
+		} else {
+			reversedArray = new char[(charArray.length - 1) / 2];
+		}
+		for (int i = charArray.length - 1; i >= Math.ceil((double) charArray.length / 2); i--) {
+			reversedArray[charArray.length - i - 1] = charArray[i];
+		}
+		for (int i = 0; i < reversedArray.length; i++) {
+			if (reversedArray[i] != charArray[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+	public static boolean isNumberPalindrom(int inputNumber) {
+		if(inputNumber <  0) {
+			return false;
+		}
+		
+		int originalNumber = inputNumber;
+		int reversedNumber = 0;
+				
+		while(inputNumber > 0) {
+			int digit = inputNumber % 10;
+			reversedNumber = reversedNumber * 10 + digit; 
+			inputNumber = inputNumber / 10;
+		}
+		
+		return originalNumber == reversedNumber;
+	}
+}
---
+++ src/DSA_Easy/TwoSum.java
@@ -0,0 +1,65 @@
+package DSA_Easy;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class TwoSum {
+
+	public static void main(String[] args) {
+		
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.print("please enter array size: "); 
+		int arrayLength = keyboard.nextInt();
+		
+		int[] nums = new int[arrayLength];
+		
+		for(int i = 0; i < arrayLength; i++) {			
+			System.out.print("Enter numbers " + i + " : ");
+			nums[i] = keyboard.nextInt();			
+		}
+		System.out.print("Enter target number: ");
+		int target = keyboard.nextInt();
+		
+		int[] resultArray = twoSumHash(nums, target);		
+		
+		System.out.print(Arrays.toString(resultArray));		
+
+	}
+	 public static int[] twoSum(int[] nums, int target) {
+		 
+		 int index1 = 0;
+		 int index2 = 0;
+		 
+		 for(int i = 0; i < nums.length; i++) {
+			 for(int j = i + 1; j < nums.length; j++ ) {
+				 if(nums[j] + nums[i] == target) {
+					 index1 = i;
+					 index2 = j;
+				 }				 
+			 }
+		 }
+		 int[] resultArray = {index1, index2};		
+		 
+		 return resultArray;
+	 }
+	 public static int[] twoSumHash(int[] nums, int target) {
+		    HashMap<Integer, Integer> numToIndex = new HashMap<>();
+
+		    for (int i = 0; i < nums.length; i++) {
+		        int complement = target - nums[i]; 
+		        if (numToIndex.containsKey(complement)) {
+		            return new int[]{numToIndex.get(complement), i};
+		        }
+		        numToIndex.put(nums[i], i); 
+		    }
+		    
+
+		    // If no solution is found, return an empty array or throw an exception
+		    // based on your requirements.
+		    return new int[]{};
+		}
+
+}
