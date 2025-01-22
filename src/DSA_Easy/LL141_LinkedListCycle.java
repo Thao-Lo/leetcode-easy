eff48704f5ab4488702cd9871fad788aea1d3a05
Zavis <thezavis@gmail.com>
2025-01-23 00:13:47 +1100

    LL 141 Cycle 2nd attempt, apply Floyd's cycle detection  alg to solve the problem

--- src/DSA_Easy/LL141_LinkedListCycle.java
+++ src/DSA_Easy/LL141_LinkedListCycle.java
@@ -1,10 +1,56 @@
 package DSA_Easy;
 
+import LinkedList.Node;
+
 public class LL141_LinkedListCycle {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	}
+	//solve problem using Set
+	
+	// O1 memory
+	//Floyd's Cycle detection alg - tho rua
+	public static boolean hasCycle(Node head) {
+		if (head == null || head.next == null) {
+			return false;
+		}
+
+		Node oneStep = head;
+		Node twoStep = head;
+		while (twoStep != null && twoStep.next != null) {
+			//1 node at a time
+			oneStep = oneStep.next;
+			//2 node at a time
+			twoStep = twoStep.next.next;
+			//if there is a cycle, twoStep will meet oneStep at next round
+			if(oneStep == twoStep) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+	
+	//Time Limit error
+	public static boolean hasCycle1(Node head) {
+		if (head == null || head.next == null) {
+			return false;
+		}
+
+		Node current = head;		
+		while (current != null && current.next != null) {
+			Node temp = head;
+			while (temp != current) {
+				if(temp == current.next) {
+					return true;
+				}
+				temp = temp.next;
+			}
+			current = current.next;
+		}
+
+		return false;
 	}
 
 }
