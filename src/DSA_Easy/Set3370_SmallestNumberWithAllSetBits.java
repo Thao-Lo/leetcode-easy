64592505447a7c48c249004e7c2ef11452d8185c
Zavis <thezavis@gmail.com>
2025-01-24 00:30:42 +1100

    Binary Set3370_SmallestNumberWithAllSetBits, 2nd attempt, using bitwise shifting x = 1 << i -1

--- src/DSA_Easy/Set3370_SmallestNumberWithAllSetBits.java
+++ src/DSA_Easy/Set3370_SmallestNumberWithAllSetBits.java
@@ -7,28 +7,37 @@ public class Set3370_SmallestNumberWithAllSetBits {
 	public static void main(String[] args) {
 		Scanner keyboard = new Scanner(System.in);
 		System.out.print("n: ");
+		// 1 <= n <= 1000
 		int n = keyboard.nextInt();
 		
 		int x = smallestNumber1(n);
 		System.out.print("Output: " + x);
 	}
-	
+
 	// loop
-	 public static int smallestNumber1(int n) {
-		 //loop through k:  x = 2^k - 1	
-		 int x = 0;
-		 for (int i = 0; i < 10; i++) {
-			 x = (int) (Math.pow(2, i) - 1);
-			 if(x >= n) {
-				 return x;
-			 }
-		 }
-	        return n;
-	    }
-	 
-	 //Bitwise shifting
-	 public static int smallestNumber2(int n) {
+	public static int smallestNumber1(int n) {
+		// loop through k: x = 2^k - 1
+		int x = 0;
+		for (int i = 0; ; i++) {
+			x = (int) (Math.pow(2, i) - 1);
+			if (x >= n) {
+				return x;
+			}
+		}
 		
-		 return n;
-	 }
+	}
+
+	// Bitwise shifting - shifting 1 in binary 0001 / 0010
+	public static int smallestNumber2(int n) {
+		if (n == 1) {
+			return n;
+		}
+		int x = 0;
+		for (int i = 0;; i++) {
+			x = (1 << i) - 1;
+			if (x >= n) {
+				return x;
+			}
+		}
+	}
 }
