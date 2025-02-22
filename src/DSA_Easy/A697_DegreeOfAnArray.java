3d9ad67e2cab98ebca775207e6a7181440810dd8
Zavis <thezavis@gmail.com>
2025-02-23 00:02:49 +1100

    A697 Degree of An Array, int num = nums[i] instead of keep getting value from the array

--- src/DSA_Easy/A697_DegreeOfAnArray.java
+++ src/DSA_Easy/A697_DegreeOfAnArray.java
@@ -6,10 +6,10 @@ import java.util.Map;
 public class A697_DegreeOfAnArray {
 
 	public static void main(String[] args) {
-		 int[] nums1 = {1, 2, 2, 3, 1};
-	        System.out.println(findShortestSubArray(nums1));
-	        int[] nums2 = {1, 2, 2, 3, 1, 4, 2};
-	        System.out.println(findShortestSubArray(nums2));
+		int[] nums1 = { 1, 2, 2, 3, 1 };
+		System.out.println(findShortestSubArray(nums1));
+		int[] nums2 = { 1, 2, 2, 3, 1, 4, 2 };
+		System.out.println(findShortestSubArray(nums2));
 	}
 
 	public static int findShortestSubArray(int[] nums) {
@@ -21,40 +21,37 @@ public class A697_DegreeOfAnArray {
 		Map<Integer, Integer> first = new HashMap<>(); // count 1st time appear
 		Map<Integer, Integer> last = new HashMap<>(); // count last time appear
 		int maxDegree = 0;
-		int minLength = Integer.MAX_VALUE; //(2^32)
-		
+		int minLength = Integer.MAX_VALUE; // (2^32) or 50 001 
+
 		for (int i = 0; i < nums.length; i++) {
+			int num = nums[i];
 			// number and its degree
-			count.put(nums[i], count.getOrDefault(nums[i], 0) + 1);
-			//number and its 1st index
-			first.putIfAbsent(nums[i], i);
-			//number and its last index
-			last.put(nums[i], i);
+			count.put(num, count.getOrDefault(num, 0) + 1);
+			// number and its 1st index
+			first.putIfAbsent(num, i);
+			// number and its last index
+			last.put(num, i);
 		}
-		for( int num : count.keySet()) {
+		
+		for (int num : count.keySet()) {
 			int degree = count.get(num);
-			int length = last.get(num) - first.get(num) + 1; //array length need to + 1
-			
-			if(degree > maxDegree) {
-				maxDegree = degree; //get the max degree 4
+			int length = last.get(num) - first.get(num) + 1; // array length need to + 1
+
+			if (degree > maxDegree) {
+				maxDegree = degree; // get the max degree 4
 				minLength = length; // set minLength as a length of that number 3
-			}else if( degree == maxDegree) { 
-				// degree 2(4) = 3(4) 
-				//compare length or minLength = Math.min(minLength, length)
-				if(minLength > length) {
-					minLength = length;
-				}
-			}			
+			} else if (degree == maxDegree) {
+				// degree 2(4) length 4 & 3(4) length 3 => 3 
+				minLength = Math.min(minLength, length);
+			}
 		}
 		return minLength;
 	}
-}	
+}
 
-		/*
-		 * - find the degree + count degree - + store position - store number - 1
-		 * position and last position: length of subarray - same largest degree with
-		 * smallest length [0,5,4,5] -> degree 3 - beginning and end 7 1 - beginning and
-		 * end 6 -> 6 [[1,5], [5,6], [5,3]]
-		 */
-	
-
+/*
+ * - find the degree + count degree - + store position - store number - 1
+ * position and last position: length of subarray - same largest degree with
+ * smallest length [0,5,4,5] -> degree 3 - beginning and end 7 1 - beginning and
+ * end 6 -> 6 [[1,5], [5,6], [5,3]]
+ */
