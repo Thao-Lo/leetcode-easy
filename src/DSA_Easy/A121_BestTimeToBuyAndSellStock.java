9d8a67a28bd61f99640c21c2f8b97ef7dc3137fe
zavis <thezavis@gmail.com>
2025-05-13 23:56:12 +1000

    Array 121 - Best Time to buy and sell stock - V2

--- src/DSA_Easy/A121_BestTimeToBuyAndSellStock.java
+++ src/DSA_Easy/A121_BestTimeToBuyAndSellStock.java
@@ -1,5 +1,38 @@
 package DSA_Easy;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class A121_BestTimeToBuyAndSellStock {
 
+	public int maxProfit(int[] prices) {
+		if (prices.length == 1) {
+			return 0;
+		}
+		int min = prices[0];
+		int max = prices[0];
+		Map<String, Integer> map = new HashMap<>();
+		map.put("max", 0);
+		map.put("min", 0);
+		for (int i = 1; i < prices.length; i++) {
+			if (prices[i] >= max) {
+				max = prices[i];
+				map.put("max", i);
+			}
+			if (prices[i] <= min) {
+				min = prices[i];
+				map.put("min", i);
+				if (map.get("max") < i) {
+					max = 0;
+					map.put("max", 0);
+				}
+			}
+		}
+		if (map.get("max") <= map.get("min")) {
+			return 0;
+		}
+		int price = prices[map.get("max")] - prices[map.get("min")];
+
+		return price;
+	}
 }
