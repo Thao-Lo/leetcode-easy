0ec9d32807081b1861bd30c8a576016eaf0ea5ae
zavis <thezavis@gmail.com>
2025-05-15 23:05:13 +1000

    Array -OPTIMIZE - using Set, return true if found duplicate

--- src/DSA_Easy/A217_ContainsDuplicate.java
+++ src/DSA_Easy/A217_ContainsDuplicate.java
@@ -1,5 +1,34 @@
 package DSA_Easy;
 
-public class A217_ContainsDuplicate {
+import java.util.HashSet;
+import java.util.Set;
 
+public class A217_ContainsDuplicate {
+	
+	//return true if any values appear twice, else false
+	public boolean containsDuplicate(int[] nums) {
+		Set<Integer> set = new HashSet<>(nums.length); //avoid set keeps resizing
+		for(int num: nums) {
+			if(set.contains(num)) {
+				return true;
+			}
+			set.add(num);		
+		}
+				
+		return false;
+	}
+	
+	//OPTIMIZE for large capacity
+	public boolean containsDuplicateTwo(int[] nums) {
+		Set<Integer> set = new HashSet<>(nums.length); //avoid set keeps resizing (after 75%)
+		for(int num: nums) {
+			// set can add num, if(false) -> set.add(num)
+			//check and add value at the same time 
+			if(!set.add(num)) {
+				return true;
+			}
+		}				
+		return false;
+	}
+	
 }
