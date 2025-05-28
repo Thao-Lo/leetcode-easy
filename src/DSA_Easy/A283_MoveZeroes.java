00beacddbaa881a9d885bb6cfce0d0cef2546377
zavis <thezavis@gmail.com>
2025-05-29 00:02:27 +1000

    A283 shift all zeroes to the end of an arary, V2 - correct

--- src/DSA_Easy/A283_MoveZeroes.java
+++ src/DSA_Easy/A283_MoveZeroes.java
@@ -1,5 +1,27 @@
 package DSA_Easy;
 
 public class A283_MoveZeroes {
+	
+	//shift all Zeroes to the end & keep the order of numbers
+	public void moveZeroes(int[] nums) {
+		// 2 pointers first/0 second/1		
+		if(nums == null || nums.length <= 1) return;
+		
+		int fir = 0;
+		int sec = 1;
+		
+		while(sec < nums.length) {
+			if(nums[fir] != 0) {
+				fir++;
+			}
+			else if(nums[sec] != 0){				
+				nums[fir] = nums[sec];
+				nums[sec] = 0;
+				fir++;
+				
+			}
+			sec++;	
+		}
+	}
 
 }
