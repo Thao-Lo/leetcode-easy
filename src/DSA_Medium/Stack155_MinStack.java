ce7bd1883fd188ed7869f8592260fbeab1a6b99b
zavis <thezavis@gmail.com>
2025-06-01 23:52:39 +1000

    Med - Stack - 155 MinStack, create constructor, methods: push, pop, top and getMin

---
+++ src/DSA_Medium/Stack155_MinStack.java
@@ -0,0 +1,25 @@
+package DSA_Medium;
+
+public class Stack155_MinStack {
+	// O1 time complexity for each function
+	// MinStack obj = new MinStack();
+	public Stack155_MinStack() {
+        super();
+    }
+
+	public void push(int val) {
+		
+	}
+
+	public void pop() {
+
+	}
+
+	public int top() {
+
+	}
+
+	public int getMin() {
+
+	}
+}
