eb69fd9d8d0075c721252b188a32303dd7b001c5
zavis <thezavis@gmail.com>
2025-07-01 21:08:57 +1000

    Recursive - swap then recursive

--- src/Binary_Search_Tree/B226_Invert_Binary_Tree.java
+++ src/Binary_Search_Tree/B226_Invert_Binary_Tree.java
@@ -2,4 +2,33 @@ package Binary_Search_Tree;
 
 public class B226_Invert_Binary_Tree {
 
+	public TreeNode invertTree(TreeNode root) {
+		if (root == null) {
+			return null;
+		}
+
+		// recursive invert sub tree
+		TreeNode left = invertTree(root.left);
+		TreeNode right = invertTree(root.right);
+
+		// Swap
+		root.left = right;
+		root.right = left;
+
+		return root;
+	}
+
+	public TreeNode invertTree1(TreeNode root) {
+		if (root == null) {
+			return null;
+		}
+		TreeNode T2 = root.left;
+		root.left = root.right;
+		root.right = T2;
+
+		root.left = invertTree(root.left);
+		root.right = invertTree(root.right);
+
+		return root;
+	}
 }
