6b3fe705dd5d57d5c4b692c4b407e04e64aa4a98
zavis <thezavis@gmail.com>
2025-07-03 22:46:33 +1000

    Medium - for each number in nums, add it to all existing subsets to form a new ones

--- src/DSA_Medium/S78_Subsets.java
+++ src/DSA_Medium/S78_Subsets.java
@@ -1,29 +1,30 @@
 package DSA_Medium;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.ArrayList; 
 
-// input [0,1,2] -> output [] [0] [1] [2] [0,1] ..
-//
 public class S78_Subsets {
+	// input [0,1,2] -> output [] [0] [1] [2] [0,1] ..
+	
 	public List<List<Integer>> subsets(int[] nums) {
 
 		List<List<Integer>> list = new ArrayList<>();
-
-		//loop through nums[]
-		// 
-		for(int i = 0; i < nums.length; i++) {
+		//start with empty list [ [] ]
+		list.add(new ArrayList<Integer>());
+		
+		for (int i = 0; i < nums.length; i++) {
+			int size = list.size(); //loop over the current size
 			
-			
-			for(int j = 0; j <= i; j++) {
-				List sub = new ArrayList<>();
-				
-				
+			for (int j = 0; j < size; j++) {
+				// make a copy of each sublist
+				List<Integer> sub = new ArrayList<>(list.get(j));
+				//[ [], [] add 0, [] add 1, [0] add 1 -> [0,1] ]
+				sub.add(nums[i]);				
+				list.add(sub); //size keep increasing
 			}
 		}
-		
-		
-		return null;
 
+		return list;
 	}
+
 }
