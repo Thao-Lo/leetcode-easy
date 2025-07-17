da6731bd4f83c1857584d4194dd2faf10b4ecd07
zavis <thezavis@gmail.com>
2025-07-17 23:46:27 +1000

    Easy 1 - return indices of 2 numbers such that they add up to target

---
+++ src/DSA_Easy/A1_TwoSum.java
@@ -0,0 +1,25 @@
+package DSA_Easy;
+
+import java.util.HashMap;
+
+public class A1_TwoSum {
+	
+	// return indices of the num1 + num2 = target
+	public int[] twoSum(int[] nums, int target) {
+		// hashmap: index, target - nums[i] -> 7:1 2.2 > taget, skip, 3:3 4:2 2:4
+		
+		HashMap<Integer, Integer> map = new HashMap<>();
+		int[] result = new int[2];
+		
+		for(int i = 0; i< nums.length; i++) {
+			if(map.containsKey(nums[i])) {
+				result[0] = map.get(nums[i]); // 1st index == value 
+				result[1] = i; //2nd index == key
+				break;				
+			}
+			map.put(target - nums[i], i);
+			
+		}
+		return result;
+	}
+}
