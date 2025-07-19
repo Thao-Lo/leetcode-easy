964213e98e9105a693a766dd30336f7fd8e49d59
zavis <thezavis@gmail.com>
2025-07-19 23:08:02 +1000

    Easy 1 - Two Sum version2 using calculate remaining 1st, then check and add to map later

--- src/DSA_Easy/A1_TwoSum.java
+++ src/DSA_Easy/A1_TwoSum.java
@@ -22,4 +22,22 @@ public class A1_TwoSum {
 		}
 		return result;
 	}
+	
+	public int[] twoSum2(int[] nums, int target) {	
+		
+		HashMap<Integer, Integer> map = new HashMap<>();
+		int[] result = new int[2];
+		
+		for(int i = 0; i< nums.length; i++) {
+			int remaining = target - nums[i];
+			if(map.containsKey(remaining)) { //key == nums[i] 7:1 - 2:2 t=9 9-2==7
+				result[0] = map.get(remaining);
+				result[1] = i;
+			}
+			map.put(nums[i], i);
+			
+		}
+		return result;	
+	}
+	
 }
