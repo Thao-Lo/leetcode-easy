46faa9dcdfb80cd2c49e2cb9bc88106132424a1b
zavis <thezavis@gmail.com>
2025-07-21 22:54:14 +1000

    Medium 53 - apply with sum of negative numbers

--- src/DSA_Easy/A53_MaximumSubarray.java
+++ src/DSA_Easy/A53_MaximumSubarray.java
@@ -7,7 +7,11 @@ public class A53_MaximumSubarray {
 	// nums = [-2,1,-3,4,-1,2,1,-5,4] || nums = [5,4,-1,7,8]
 	
 	//dynamic programming 
+	//each step: extend the current array? or start a new subarray
 	public int maxSubArray(int[] nums) {
+		if(nums.length == 1) {
+			return nums[0];
+		}
 		int localMax = 0;
 		int globalMax = 0;
 		
@@ -16,10 +20,27 @@ public class A53_MaximumSubarray {
 			if(localMax >= globalMax) {
 				globalMax = Math.max(localMax, globalMax); //compare with current globaMax
 				
-			}else {
-				localMax = 0; //localmax < 0
+			}else { //localMax < 0
+				//localMax = 0; 
+				localMax = 0; 
 			}
 		}		
 		return globalMax;
 	}
+	
+	//include negative sum
+	public int maxSubArray2(int[] nums) {		
+		if(nums.length == 1) {
+			return nums[0];
+		}
+		int localMax = 0;
+		int globalMax = nums[0];
+		
+		for(int i = 1; i < nums.length; i++) {		
+			localMax = Math.max(nums[i], localMax + nums[i]); 
+			globalMax = Math.max(localMax, globalMax); 			
+		}		
+		return globalMax;
+	}
 }
+
